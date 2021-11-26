a8cc6897b6fae96275b859d61468aaef82ee8149
Leon-MI <42640946+Leon-MI@users.noreply.github.com>
2021-11-26 12:08:53 +0100

    Implemented the bully election algorithm.

--- lab2/server.py
+++ lab2/server.py
@@ -89,26 +89,33 @@ try:
         try:
             new_entry = request.forms.get('entry')
             
+            success = False
             # Propagate action:
             if (node_id == leader_id):
             	print("Propagating to replicas")
                 element_id = 0
                 while (element_id in board):
                     element_id = element_id + 1
-                add_new_element_to_store(element_id, new_entry)
+                success = add_new_element_to_store(element_id, new_entry)
 
-                # Propagate to all the replicas
-                thread = Thread(target=propagate_to_vessels,
-                                args=('/propagate/ADD/{}'.format(element_id), {'entry': new_entry}, 'POST'))
-                thread.daemon = True
-                thread.start()
+                if success:
+                	# Propagate to all the replicas
+                	thread = Thread(target=propagate_to_vessels,
+                		args=('/propagate/ADD/{}'.format(element_id), {'entry': new_entry}, 'POST'))
+                	thread.daemon = True
+                	thread.start()
             else:
             	print("Propagating to leader")
-                thread = Thread(target=propagate_to_leader,
-                                args=('/propagate/ADD/{}'.format(-1), {'entry': new_entry}, 'POST'))
-                thread.daemon = True
-                thread.start()
-            return '<h1>Successfully added entry</h1>'
+            	success = propagate_to_leader(
+            		path='/propagate/ADD/-1',
+            		payload={'entry': new_entry},
+            		req='POST'
+            	)
+
+            if success:
+            	return '<h1>Successfully added entry</h1>'
+            else:
+            	return '<h1>Failed, please retry in a few seconds</hi>'
         except Exception as e:
             print(e)
         return False
@@ -126,19 +133,16 @@ try:
             
             #call either delete or modify
             if (int(delete_option) == 0):
-              #  modify_element_in_store(element_id, entry, False)
                 action = "MODIFY"
-            
-            if (int(delete_option) == 1):
-           #     delete_element_from_store(element_id, False)
+            elif (int(delete_option) == 1):
                 action = "DELETE"
             
             #propagate to other nodes
-            
+            success = False
             if (node_id == leader_id):
                 if action == "MODIFY":
                 	success = modify_element_in_store(element_id, entry, False)
-                else:
+                elif action == "DELETE":
                 	success = delete_element_from_store(element_id, False)
 
                 if success: # Do not propagate if the action failed
@@ -146,15 +150,18 @@ try:
                                 	args=('/propagate/{}/{}'.format(action, element_id), {'entry': entry}, 'POST'))
                 	thread.daemon = True
                 	thread.start()
-                else:
-                	return False
             else:
             	print("Propagating to leader")
-                thread = Thread(target=propagate_to_leader,
-                                args=('/propagate/{}/{}'.format(action, element_id), {'entry': entry}, 'POST'))
-                thread.daemon = True
-                thread.start()
-            return '<h1>Successfully ' + action + ' entry</h1>'
+            	success = propagate_to_leader(
+            		path='/propagate/{}/{}'.format(action, element_id),
+            		payload={'entry': entry},
+            		req='POST'
+            	)
+
+            if success:
+            	return '<h1>Successfully ' + action + ' entry</h1>'
+            else:
+            	return '<h1>Failed, please retry in a few seconds</h1>'	
         except Exception as e:
             print(e)
         return False
@@ -179,7 +186,7 @@ try:
         elif action == "MODIFY":
             modify_element_in_store(element_id, entry, True)
         # Delete the entry from the board
-        elif "DELETE":
+        elif action == "DELETE":
             delete_element_from_store(element_id, True)
         else:
             return False
@@ -192,6 +199,21 @@ try:
 
         return '<h1>Successfully propagated ' + action + '</h1>'
 
+
+    @app.get('/election/started')
+    def election_started():
+    	thread = Thread(target=start_election)
+    	thread.daemon = True
+    	thread.start()
+    	return '<h1>Election started</h1>'
+
+
+    @app.post('/election/success/<leader:int>')
+    def election_successful(leader):
+     	global leader_id
+     	leader_id = leader
+     	print('NEW LEADER IS : {}'.format(leader_id))
+     	return '<h1>Leader changed</h1>'
 
     # ------------------------------------------------------------------------------------------------------
     # DISTRIBUTED COMMUNICATIONS FUNCTIONS
@@ -206,14 +228,14 @@ try:
                 res = requests.get('http://{}{}'.format(vessel_ip, path))
             else:
                 print('Non implemented feature!')
-            # result is in res.text or res.json()
-            #print(res.text)
+
             if res.status_code == 200:
                 success = True
         except Exception as e:
         	print(e)
         return success
-    
+
+	
     def propagate_to_leader(path, payload = None, req = 'POST'):
         global vessel_list, node_id, leader_id
         
@@ -221,8 +243,15 @@ try:
             
         if not success:
             print("\n\nCould not contact leader : id={}, ip={}\n\n".format(leader_id, vessel_list[str(leader_id)]))
+            print("Let's initiate LEADER ELECTION!")
 
-    
+            thread = Thread(target=start_election)
+            thread.daemon = True
+            thread.start()
+
+        return success
+
+
     def propagate_to_vessels(path, payload = None, req = 'POST'):
         """
         Propagate a message to all the replicas.
@@ -236,7 +265,39 @@ try:
                 if not success:
                     print("\n\nCould not contact vessel {}\n\n".format(vessel_id))
 
-        
+    def start_election():
+    	global vessel_list, node_id, leader_id
+    	is_new_leader = True # Assume that we are the new leader
+    	for vessel_id, vessel_ip in vessel_list.items():
+    		if int(vessel_id) > node_id and is_new_leader:
+    			try:
+	    			res = contact_vessel(
+	    				vessel_ip=vessel_ip,
+	    				path='/election/started',
+	    				req='GET'
+	    			)
+	    			if res == True:
+	    				is_new_leader = False
+	    				break
+    			except Exception as e:
+    				print("Could not contact node {} during election process".format(vessel_id))
+    				print(e)
+
+    	# if is_new_leader == True, we could not contact nodes with higher id
+    	if is_new_leader:
+     		leader_id = node_id
+     		thread = Thread(target=propagate_new_leader, args=(node_id,))
+     		thread.daemon = True
+     		thread.start()
+     	return is_new_leader
+
+    def propagate_new_leader(id):
+    	global vessel_list, node_id
+
+    	for vessel_id, vessel_ip in vessel_list.items():
+    		if vessel_id != str(node_id):
+    			contact_vessel(vessel_ip, '/election/success/' + str(id), req='POST')
+
     # ------------------------------------------------------------------------------------------------------
     # EXECUTION
     # ------------------------------------------------------------------------------------------------------
