0b64c25303e6308b0bd7a2a204621e4a69751777
Maxxter96 <62884472+Maxxter96@users.noreply.github.com>
2022-01-10 18:31:30 +0100

    Cleaned up directory and added commenting

--- lab4/server/server.py
+++ lab4/server/server.py
@@ -18,14 +18,13 @@ try:
 
     #board stores all message on the system 
     board = {0 : "Welcome to Distributed Systems Course"} 
-    vote_dict = {}
-    vectors = {}
-    byz_node = False
-    no_loyal = 3
-    no_total = 4
-    result_vect = []
-    result_action = ""
-
+    vote_dict = {}   #Vote vector storing all incoming votes in phase 1
+    vectors = {}   #Dictionary storing all incoming vote vectors in phase 2
+    byz_node = False   #Boolean describing if a node is byzantine or not
+    no_loyal = 3  #Number of loyal nodes in network
+    no_total = 4  #Total number of nodes in network
+    result_vect = []  #Final result vector
+    result_action = ""  #Final result action
 
     # ------------------------------------------------------------------------------------------------------
     # ROUTES
@@ -39,12 +38,7 @@ try:
         return template('server/index.tpl', board_title='Vessel {}'.format(node_id),
                 board_dict=sorted({"0":board,}.iteritems()), members_name_string='Léon Michalski, Max Sonnelid, Elias Estribeau')
 
-    @app.get('/board')
-    def get_board():
-        global board, node_id
-        print(board)
-        return template('server/boardcontents_template.tpl',board_title='Vessel {}'.format(node_id), board_dict=sorted(board.iteritems()))
-
+    #Displays the current result vector together with the resulting action on the website
     @app.get('/vote/result')
     def get_board():
         global result_vect, result_action
@@ -52,20 +46,24 @@ try:
 
     # NEW BYZANTINE ALGORITHM FUNCTIONS
 
+    #Method called when user presses either 'Attack', 'Retreat' or 'Byzantine' in the web page interface
+    #If loyal node, store your own vote in the vote vector, propagate your vote to the other nodes and continue behavior as loyal node (see 'loyal_behaviour()')
+    #If byzantine node, continue behaviour as byzantine node (see 'byzantine_behaviour()')
+    
     @app.post('/vote/<action>')
     def client_vote_received(action):
         global vote_dict, node_id, byz_node
         try:
             print('The performed action is: ' + action)
-            if int(node_id) not in vote_dict:
+            if (int(node_id) not in vote_dict) and (not byz_node):   #A node is only allowed to vote / set byzantine behaviour once
                 if action == 'byzantine':
                    byz_node = True
                    byzantine_behavior()
                    return '<h1>Successfully set as Byzantine</h1>'
                 if action == 'attack':
-                    vote_dict[int(node_id)] = True
+                    vote_dict[int(node_id)] = True  #True represents attack
                 if action == 'retreat':
-                    vote_dict[int(node_id)] = False
+                    vote_dict[int(node_id)] = False  #False represents retreat
 
                 print('The current vote vector looks like: ' + str(vote_dict))
 
@@ -79,10 +77,11 @@ try:
                 return '<h1>Successfully sent vote</h1>'
         except Exception as e:
             print(e)
-        return '<h1>Failed, please retry in a few seconds</h1>'
+        return '<h1>You have already voted!</h1>'
 
-         #With this function you handle requests from other nodes like add modify or delete
-
+    #Method called when node receives a vote from another node
+    #Stores the vote in its vote vector and continues either byzantine or loyal behavior depending on byz_node value (see 'loyal_behaviour()' resp. 'byzantine_behaviour()')
+ 
     @app.post('/propagate/VOTE/')
     def vote_propagation_received():
         global vote_dict, node_id, byz_node, no_total
@@ -92,7 +91,7 @@ try:
             entry = str_to_bool(entry)
             rec_id = int(request.forms.get('node_id'))
 
-            if rec_id not in vote_dict:
+            if rec_id not in vote_dict:  #Do not store a vote from a node if you already have a vote from that node
                 vote_dict[rec_id] = entry
 
                 print('The current vote vector of size {} is: {}'.format(len(vote_dict), vote_dict))
@@ -107,6 +106,8 @@ try:
         
         return '<h1>Vote propagation failed</h1>'
 
+    #Method for receiving a propagated vote vector (only used by loyal nodes)
+    #
     @app.post('/propagate/VOTE/list')
     def list_propagation_received():
         global vote_dict, node_id, byz_node, no_total, no_loyal, vectors, result_vect, result_action
@@ -175,12 +176,14 @@ try:
         return '<h1>Vector propagation failed</h1>'
 
 
-    # Byzantine behavior:
+    #Method for implementing behaviour as byzantine node
+    #When votes from all loyal nodes have been received, compute single votes (round 1) to send to the loyal nodes and send them
+    #When vote vectors from all loyal nodes have been received, compute vote vectors (round 2) to send to the loyal nodes and send them
     def byzantine_behavior():
         global vote_dict, node_id, byz_node, no_loyal, no_total, vessel_list
         if (len(vote_dict) == no_loyal) and (byz_node == True):
-            # Send the votes from round 1 to every other loyal processes
-            byz_votes = compute_byzantine_vote_round1(no_loyal,no_total,False)
+        	#Send votes from round 1 to all loyal nodes
+            byz_votes = compute_byzantine_vote_round1(no_loyal,no_total,True)
             loyal_count = 0
             for vessel_id, vessel_ip in vessel_list.items():
                 if int(vessel_id) != node_id: # don't propagate to yourself
@@ -189,8 +192,8 @@ try:
                     thread.start()
                     loyal_count += 1
 
-            # Send the vector votes to every other loyal processes
-            byz_vects = compute_byzantine_vote_round2(no_loyal,no_total,False)
+            # Send the vote vectors from round 2 to every other loyal process
+            byz_vects = compute_byzantine_vote_round2(no_loyal,no_total,True)
             loyal_count = 0
             for vessel_id, vessel_ip in vessel_list.items():
                 if int(vessel_id) != node_id: # don't propagate to yourself
@@ -203,20 +206,24 @@ try:
                     thread.start()
                     loyal_count += 1
 
+    #Method for implementing behaviour as loyal node
+    #When the vote vector is full, add it to our own vote vector dictionary ('vectors') and propagate the vote vector to all other nodes
     def loyal_behavior():
         global vote_dict, no_total, byz_node, vectors, node_id
         if len(vote_dict) == no_total and not byz_node:
-            # Create our vote vector, save it and propagate it
+            # Transform our vote vector to correct format (array)
             vect = []
             for i in range(1, no_total + 1):
                 vect.append(vote_dict[i])
             vect = [str(b) for b in vect]
             vect = ",".join(vect)
 
+            #Save vote vector to own vote vector dictionary
             vect_to_add = vect.split(",")
             vect_to_add = [str_to_bool(vote) for vote in vect_to_add]
             vectors[node_id] = vect_to_add
 
+            #Propagate vote vector to other nodes
             print("LOYAL propagating vector = {}".format(vect))
             thread = Thread(target=propagate_to_vessels,
                             args=('/propagate/VOTE/list', {'vector': vect, 'node_id': node_id}, 'POST'))
@@ -265,7 +272,7 @@ try:
           result_vectors.append([not on_tie]*no_total)
       return result_vectors
     
-    #------------------------------------------------------------------------------------------------------
+    #Method for converting a string value to a boolean value
     def str_to_bool(str):
         return str in ["True", "true", "1"]
     # ------------------------------------------------------------------------------------------------------
